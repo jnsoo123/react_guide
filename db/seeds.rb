5f58379a23367632caf39bdc011712ac8213c7d8
John Neil Soo <neil.soo@adish.co.jp>
2016-09-28 09:00:37 +0800

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times { |x| Item.create(name: "Item#{x+1}", description: "Description#{x+1}") }
